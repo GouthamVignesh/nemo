1040ad72fc23b53dcfba80648b11f7aa99213f76
Goutham Vignesh <goutham260898@gmail.com>
2018-12-05 06:21:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,12 @@ def makeWebhookResult(speech):
     return {
         "speech": speech,
         "displayText": speech,
+        "messages": {
+            "type": 1,
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+        }
         "source": "Build conversational interface for your app in 10 minutes."
     }
 
