a3064536406e329e8408787f653deb5b27adfe06
Goutham Vignesh <goutham260898@gmail.com>
2019-02-21 13:21:02 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,6 @@
+
+def database():
+    from firebase import firebase
+    firebase = firebase.FirebaseApplication('https://nemo-bot-9ae9c.firebaseio.com/A/')
+    result = firebase.get('ABDOMINAL PAIN', None)
+    return result
