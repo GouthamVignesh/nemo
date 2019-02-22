4e15901cabb2db93044b9e30d7e0906c6f08cbe2
Goutham Vignesh <goutham260898@gmail.com>
2019-02-22 13:39:41 +0530

    Add files via upload

---
+++ hospitalfind.py
@@ -0,0 +1,25 @@
+from googleplaces import GooglePlaces, types
+
+
+def hospitalfind() :
+    YOUR_API_KEY = 'AIzaSyDkfWiMGRhbIWJwk578id9e2X2Nhd7n-90'
+
+    google_places = GooglePlaces(YOUR_API_KEY)
+
+    query_result = google_places.nearby_search(
+        location='Coimbatore, Tamil Nadu', keyword='hospital',
+        radius=2000, types=[types.TYPE_HOSPITAL])
+
+    strr = " "
+    flag = 0
+    for place in query_result.places:
+        place.get_details()
+        strr = strr + "\n Name :" + str(place.name) + "\n Address:" + str(place.formatted_address) + "\n Phone Number :" + str(place.international_phone_number) + "\n Map Url :" + str(place.url) + "\n Web Link :" + str(place.website) + "\n Ratings:" + str(place.rating) +"\n \n"
+
+        flag = flag+1
+        if flag == 5:
+            break
+
+    return strr
+    #returns nearby Pharmacy
+    # dependency : python-google-places 1.4.1
