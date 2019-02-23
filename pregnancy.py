4681b3c244450e66d620d5e9fe382b0c95c26464
Goutham Vignesh <goutham260898@gmail.com>
2019-02-23 12:39:59 +0530

    Update pregnancy.py

--- pregnancy.py
+++ pregnancy.py
@@ -1,21 +1,21 @@
 def pregnancy(data):
     from firebase import firebase
     firebase = firebase.FirebaseApplication('https://preganency.firebaseio.com/PREGANENCY/')
-    my_input = data
-    if (my_input >= 1) and (my_input <= 13) :
+    my = int(data)
+    if (my >= 1) and (my <= 13) :
         result = firebase.get('FIRST TRIMESTER (WEEKS 1 TO 13)',None)
         x = func(result)
         x=('FIRST TRIMESTER:'+'\n'+x)
         return x
 
 
-    if (my_input > 13) and (my_input <= 26) :
+    if (my > 13) and (my <= 26) :
         result = firebase.get('SECOND TRIMESTER (WEEKS 13 TO 26)',None)
         y = func(result)
         y=('SECOND TRIMESTER'+'\n'+y)
         return y
 
-    if (my_input > 26) and (my_input <= 40) :
+    if (my > 26) and (my <= 40) :
         result = firebase.get('THIRD TRIMESTER (WEEKS 27 TO 40)',None)
         z = func(result)
         z=('THIRD TRIMESTER'+'\n'+z)
@@ -28,3 +28,4 @@ def func(result):
         output += "\n" + str(result[i])
         output += "\n\n"
     return  output
+
